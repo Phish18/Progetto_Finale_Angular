17d7b253923da489cedbbd2a6a017cc1d2dda955
Phish18 <diego.pesce18@gmail.com>
2019-05-17 10:56:57 +0000

    Register

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,8 +1,11 @@
+//Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Components
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 //Routing
 import { RouterModule, Routes } from '@angular/router';
@@ -11,8 +14,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material';
+import {MatDatepickerModule, MatNativeDateModule, MatIconModule} from '@angular/material/';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const appRoutes: Routes = [
+  { path: '', component: RegisterComponent },
   { path: 'register', component: RegisterComponent }
   //{ path: '**', component: PageNotFoundComponent }
 ];
@@ -20,7 +30,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -30,7 +41,16 @@ const appRoutes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front-end/src/app/register/register.component.ts
+++ Front-end/src/app/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
+
+  controlName = new FormControl('', [
+    Validators.required
+  ]);
+
+  controlSurname = new FormControl('', [
+    Validators.required
+  ]);
+
+  controlEmail = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  controlUser = new FormControl('', [
+    Validators.required
+  ]);
+
+  controlPassword = new FormControl('', [
+    Validators.required
+  ]);
 
   constructor() { }
 
   ngOnInit() {
   }
 
+   /*myFilter = (d: Date): boolean => {
+    const day = d.getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  }*/
+
+  onSubmit(name : HTMLInputElement, surname: HTMLInputElement, date: HTMLInputElement, email: HTMLInputElement, user: HTMLInputElement, password: HTMLInputElement){
+    //nome = name.value;
+  }
 }
